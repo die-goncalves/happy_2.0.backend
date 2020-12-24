ce2bbd9df933b56b26d7a3a0d59014b5e71140e6
die-goncalves <die.goncalves1990@gmail.com>
2020-12-24 18:01:39 -0300

    GREEN should successfully upload file for a orphan hosting
    The test simulates sending a data set using a form using enctype='multipart/form-data' to the server using the 'hosting/create' route. The route will have a method that will take the data from req.body and req.files and join them.

--- src/controllers/orphan_hosting.ts
+++ src/controllers/orphan_hosting.ts
@@ -1,5 +1,5 @@
 import { Controller, Post } from '@overnightjs/core';
-import { hostingModel } from '@src/models/orphanhosting';
+import { hosting, hostingModel } from '@src/models/orphanhosting';
 import { Request, Response } from 'express';
 
 @Controller('hosting')
@@ -8,6 +8,25 @@ export class OrphanHostingController {
   public async create(req: Request, res: Response): Promise<void> {
     const orphan_hosting = new hostingModel(req.body);
     const result = await orphan_hosting.save();
+    res.status(201).send(result);
+  }
+  @Post('create')
+  public set(req: Request, res: Response): void {
+    const photo_data = new Array();
+
+    const storedData: hosting = {
+      name: req.body.name,
+      latitude: Number(req.body.latitude),
+      longitude: Number(req.body.longitude),
+      about: req.body.about,
+      instructions: req.body.instructions,
+      opening_hours: req.body.opening_hours,
+      open_on_weekends: req.body.open_on_weekends === 'true',
+    };
+
+    const photos = req.files as Express.Multer.File[];
+    photo_data.push({ filename: photos[0].filename });
+    const result = { ...storedData, pictures: photo_data };
     res.status(201).send(result);
   }
 }
