f30d5f4533a00f6f922029bce82d15b21c77b7a8
die-goncalves <die.goncalves1990@gmail.com>
2020-12-28 15:37:39 -0300

    GREEN should successfully show all orphan hosting

--- src/controllers/orphan_hosting.ts
+++ src/controllers/orphan_hosting.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post } from '@overnightjs/core';
-import { hosting, hostingModel } from '@src/models/orphanhosting';
-import { pictureModel } from '@src/models/picture';
+import { hostingModel } from '@src/models/orphanhosting';
+import { picture, pictureModel } from '@src/models/picture';
 import { Request, Response } from 'express';
 import host from '@src/view/host';
 
@@ -43,5 +43,34 @@ export class OrphanHostingController {
   public async show(req: Request, res: Response): Promise<void> {
     const result = await host.specificHosting(String(req.query._id));
     res.status(200).send(result);
+  }
+  @Get('')
+  public async index(req: Request, res: Response): Promise<void> {
+    let typeHost;
+    const allHost = new Array();
+    const foundHosting = await hostingModel.find({});
+    for (const host of foundHosting) {
+      const foundPictures = await pictureModel.find({
+        _idHosting: host._id,
+      });
+      const foundPicturesObjects = foundPictures.map((pic) => {
+        const filterPic: picture = {
+          _id: pic._id,
+          destination: pic.destination,
+          filename: pic.filename,
+        };
+        return filterPic;
+      });
+      foundPicturesObjects.length == 0
+        ? (typeHost = {
+            ...host?.toObject(),
+          })
+        : (typeHost = {
+            ...host?.toObject(),
+            pictures: foundPicturesObjects,
+          });
+      allHost.push(typeHost);
+    }
+    res.status(200).send(allHost);
   }
 }
