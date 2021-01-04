f1d56f04fe35b9ed519f50e464a7b45e76b35ef7
die-goncalves <die.goncalves1990@gmail.com>
2021-01-04 16:48:10 -0300

    Add service to encrypt the password

---
+++ src/services/auth.ts
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt';
+
+export default class AuthService {
+  public static async hashPassword(
+    password: string,
+    salt = 10
+  ): Promise<string> {
+    return await bcrypt.hash(password, salt);
+  }
+}
