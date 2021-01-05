64084511f4ab79aa23ce64cf7d8421999eb703ae
die-goncalves <die.goncalves1990@gmail.com>
2021-01-04 22:34:41 -0300

    RED should return a validation error when a field is missing
    The test has the required username field missing, when trying to save the user returns the error:
    ValidationError: user validation failure: username: The path `username` is mandatory.
    -> use try...catch

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -6,7 +6,7 @@ describe('Controllers: Users', () => {
     await userModel.deleteMany({});
   });
   describe('Create a new user', () => {
-    it('should successfully create a new user', async () => {
+    test('should successfully create a new user', async () => {
       const defaultUser = {
         username: 'John Doe',
         email: 'john@mail.com',
@@ -29,6 +29,24 @@ describe('Controllers: Users', () => {
           ...{ password: expect.any(String) },
         })
       );
+    });
+
+    test('should return a validation error when a field is missing', async () => {
+      const defaultUser = {
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const response = await global.testRequest
+        .post('/user/create')
+        .field(defaultUser);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        message:
+          'user validation failed: username: Path `username` is required.',
+        name: 'UNPROCESSABLE_ENTITY',
+      });
     });
   });
 });
