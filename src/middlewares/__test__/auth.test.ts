e79d4b3304edaf0ae2c1a479427db4b9b517b8d5
die-goncalves <die.goncalves1990@gmail.com>
2021-01-06 14:43:42 -0300

    RED should return UNAUTHORIZED if there is a problem on the token verification
    The test seeks to throw the unauthorized error when there is an invalid token. We will run the authentication middleware with its mocked arguments.
    -> reqFake that represents the request contains in the headers authorization the invalid token;
    -> resFake that represents the response contains a mock version of status and send;
    -> nextFake is a mock version of next().
    When executing the authentication middleware function, your request will contain the invalid token, the error will occur when trying to decode the token and we must send a response with status 401 and the error formatted in the response body.
    The current test run returns the error: 'JsonWebTokenError: jwt malformed'

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -14,4 +14,29 @@ describe('AuthMiddleware', () => {
     authMiddleware(reqFake, resFake, nextFake);
     expect(nextFake).toHaveBeenCalled();
   });
+  test('should return UNAUTHORIZED if there is a problem on the token verification', () => {
+    const reqFake = {
+      headers: {
+        authorization: `Bearer invalid token`,
+      },
+    };
+
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })),
+    };
+
+    const nextFake = jest.fn();
+
+    authMiddleware(reqFake, resFake as object, nextFake);
+
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      message: 'jwt malformed',
+      name: 'UNAUTHORIZED',
+    });
+  });
 });
