0a5807d7ded8b63a50d6bd5cdb162d708b64bc78
die-goncalves <die.goncalves1990@gmail.com>
2021-01-06 15:08:08 -0300

    GREEN should return UNAUTHORIZED if there is a problem on the token verification
    Add try...catch

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,15 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import AuthService from '@src/services/auth';
+import ArchetypeError from '@src/util/errors/ArchetypeError';
 
 export function authMiddleware(
   req: Partial<Request>,
   res: Partial<Response>,
   next: NextFunction
 ): void {
-  const bearer = req.headers?.authorization;
-  const token = bearer?.replace('Bearer', '').trim();
+  try {
+    const bearer = req.headers?.authorization;
+    const token = bearer?.replace('Bearer', '').trim();
 
-  const decoded = AuthService.decodeToken(token as string);
-  req.decoded = decoded;
-  next();
+    const decoded = AuthService.decodeToken(token as string);
+    req.decoded = decoded;
+    next();
+  } catch (error) {
+    res
+      .status?.(401)
+      .send(ArchetypeError.pack({ code: 401, message: error.message }));
+  }
 }
