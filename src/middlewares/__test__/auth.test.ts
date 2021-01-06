553d66200e52e632e3e0a46ead788f1294eda8b2
die-goncalves <die.goncalves1990@gmail.com>
2021-01-05 22:22:47 -0300

    RED should verify a JWT token and call the next middleware
    The test seeks to test the behavior of middleware functions in relation to the task of calling the next middleware on the stack in the authentication middleware.

---
+++ src/middlewares/__test__/auth.test.ts
@@ -0,0 +1,16 @@
+import AuthService from '@src/services/auth';
+
+describe('AuthMiddleware', () => {
+  test('should verify a JWT token and call the next middleware', () => {
+    const jwtToken = AuthService.generateToken({ data: 'fake' });
+    const reqFake = {
+      headers: {
+        authorization: `Bearer ${jwtToken}`,
+      },
+    };
+    const resFake = {};
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake, nextFake);
+    expect(nextFake).toHaveBeenCalled();
+  });
+});
