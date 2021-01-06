112d278b6f9bd1987a11f7c3f0b1261be538a0db
die-goncalves <die.goncalves1990@gmail.com>
2021-01-06 15:15:55 -0300

    GREEN should return UNAUTHORIZED if theres no token
    Same behavior as the previous test, but now the request has no token in headers authorization.

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -39,4 +39,29 @@ describe('AuthMiddleware', () => {
       name: 'UNAUTHORIZED',
     });
   });
+  test('should return UNAUTHORIZED if theres no token', () => {
+    const reqFake = {
+      headers: {
+        authorization: '',
+      },
+    };
+
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })),
+    };
+
+    const nextFake = jest.fn();
+
+    authMiddleware(reqFake, resFake as object, nextFake);
+
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      message: 'jwt must be provided',
+      name: 'UNAUTHORIZED',
+    });
+  });
 });
