2f5631eeaa9431797ed3e2f2a6f92b5995f95ac0
die-goncalves <die.goncalves1990@gmail.com>
2021-01-11 21:51:29 -0300

    RED should return FORBIDDEN if there is no role adm
    The test seeks to throw the forbidden error when there is no role adm in user. We will run the adm middleware with its mocked arguments.
    -> reqFake that represents the request contains in the decoded.id the id of user;
    -> resFake that represents the response contains a mock version of status and send;
    -> nextFake is a mock version of next().
    When executing the middleware function contain the params above, an error will occur when checking roles and we must send a response with status 403 and the error formatted in the response body.

--- test/functional/roles.test.ts
+++ test/functional/roles.test.ts
@@ -28,4 +28,38 @@ describe('RolesMiddleware', () => {
     await adm(reqFake, resFake, nextFake);
     expect(nextFake).toHaveBeenCalled();
   });
+  test('should return FORBIDDEN if there is no role adm', async () => {
+    const defaultUser = {
+      _id: new mongoose.Types.ObjectId('000000000000000000000000'),
+      username: 'John Doe',
+      email: 'john2@mail.com',
+      password: '1234',
+      role: ['usr'],
+    };
+    await new userModel(defaultUser).save();
+
+    const reqFake = {
+      decoded: {
+        _id: '000000000000000000000000',
+        iat: 0,
+        exp: 0,
+      },
+    };
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })),
+    };
+    const nextFake = jest.fn();
+
+    await adm(reqFake, resFake as object, nextFake);
+
+    expect(resFake.status).toHaveBeenCalledWith(403);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 403,
+      message: 'required administrative role.',
+      name: 'FORBIDDEN',
+    });
+  });
 });
