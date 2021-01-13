3241e5754bc06bfb94dcc9bab10e6b73ab01bf26
die-goncalves <die.goncalves1990@gmail.com>
2021-01-13 11:18:52 -0300

    RED should delete orphan hosting
    Save an orphan hosting with two pictures and send via DELETE method with query params to specify the id using the route '/hosting/delete?_id=***', which needs adm authorization, to perform delete orphan hosting and its images.

--- test/functional/orphan_hosting.test.ts
+++ test/functional/orphan_hosting.test.ts
@@ -457,5 +457,41 @@ describe('Controllers: Orphan Hosting', () => {
       );
       global.Date.now = realDateNow;
     });
+    test('should delete orphan hosting', async () => {
+      const defaultHosting = {
+        _id: new mongoose.Types.ObjectId('000000000000000000000000'),
+        name: 'sample-name',
+        latitude: -10.660795923446559,
+        longitude: -14.784882579454477,
+        about: 'sample-about',
+        instructions: 'sample-instructions',
+        opening_hours: 'sample-availableTime',
+        open_on_weekends: false,
+      };
+      const newHosting = await new hostingModel(defaultHosting).save();
+      const defaultPicture_1 = {
+        _id: new mongoose.Types.ObjectId('000000000000000000000001'),
+        _idHosting: newHosting._id,
+        destination: 'sample-destination_1',
+        filename: 'sample-filename_1',
+        size: 8888,
+      };
+      const defaultPicture_2 = {
+        _id: new mongoose.Types.ObjectId('000000000000000000000002'),
+        _idHosting: newHosting._id,
+        destination: 'sample-destination_2',
+        filename: 'sample-filename_2',
+        size: 9999,
+      };
+      const newPictures_1 = await new pictureModel(defaultPicture_1).save();
+      const newPictures_2 = await new pictureModel(defaultPicture_2).save();
+
+      const response = await global.testRequest
+        .delete('/hosting/delete')
+        .set({ authorization: `Bearer ${admtoken}` })
+        .query({ _id: newHosting._id.toHexString() });
+
+      expect(response.status).toBe(200);
+    });
   });
 });
